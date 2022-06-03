2a7d6af8edca50e0850016e68e07275969ca63c4
zakzakst <e1433104@gmail.com>
2022-06-04 04:54:10 +0900

    アラート

--- src/stories/atoms/Alert.stories.js
+++ src/stories/atoms/Alert.stories.js
@@ -39,15 +39,15 @@ export default {
     },
     // 却下可能フィールド
     dismissible: {
-      name: '却下可否',
+      name: '却下可能',
       description: 'アラートの却下が可能か',
       defaultValue: false,
       control: {
         type: 'boolean',
       },
     },
-    clickClose() {
-      console.log(this);
+    onClickDismiss: {
+      action: '閉じるボタンクリック',
     },
   },
 };
@@ -57,11 +57,40 @@ const Template = (args) => ({
   setup() {
     return { args };
   },
-  template: '<v-alert v-bind="args"><template v-slot:icon><b>link<br>aa<br>aa</b></template>文言が入ります。文言が入ります。文言が入ります。文言が入ります。文言が入ります。文言が入ります。文言が入ります。文言が入ります。文言が入ります。<div>test</div></v-alert>',
+  template: `
+    <v-alert v-bind="args">
+      <p class="-mt-1">テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。</p>
+      <p>テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。</p>
+    </v-alert>
+  `,
 });
 
 export const Default = Template.bind({});
 Default.storyName = '通常';
-Default.args = {
+
+export const Dismissible = Template.bind({});
+Dismissible.storyName = '閉じるボタン';
+Dismissible.args = {
   dismissible: true,
 };
+
+const TemplateIcon = (args) => ({
+  components: { VAlert },
+  setup() {
+    return { args };
+  },
+  template: `
+    <v-alert v-bind="args">
+      <template v-slot:icon>
+        <svg width="16" height="16" class="w-6 h-6 fill-gray-700" viewBox="0 0 16 16">
+          <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2z"/>
+        </svg>
+      </template>
+      <p class="-mt-1">テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。</p>
+      <p>テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。</p>
+    </v-alert>
+  `,
+});
+
+export const Icon = TemplateIcon.bind({});
+Icon.storyName = 'アイコン付き';
