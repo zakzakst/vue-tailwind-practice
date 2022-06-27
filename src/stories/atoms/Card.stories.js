dd99036726b5ae8cd566ad0e23d5a460dbdc5001
zakzakst <e1433104@gmail.com>
2022-06-28 06:31:51 +0900

    カード作成

--- src/stories/atoms/Card.stories.js
+++ src/stories/atoms/Card.stories.js
@@ -1,63 +1,48 @@
 import VCard from '../../components/atoms/VCard';
+import VImg from '../../components/atoms/VImg';
 
 export default {
   title: 'Atoms/Card',
   component: VCard,
-  // argTypes: {
-  //   // 表示フィールド
-  //   show: {
-  //     name: '表示',
-  //     description: '表示状態',
-  //     defaultValue: true,
-  //     control: {
-  //       type: 'boolean',
-  //     },
-  //   },
-  //   // 色フィールド
-  //   color: {
-  //     name: '色',
-  //     description: 'アラートの色',
-  //     options: [
-  //       'primary',
-  //       'secondary',
-  //       'success',
-  //       'danger',
-  //       'warning',
-  //       'info',
-  //     ],
-  //     control: {
-  //       type: 'select',
-  //       labels: {
-  //         primary: 'メインカラー',
-  //         secondary: 'セカンダリカラー',
-  //         success: '成功',
-  //         danger: '警告',
-  //         warning: '注意',
-  //         info: '補足',
-  //       },
-  //     },
-  //   },
-  //   // 却下可能フィールド
-  //   dismissible: {
-  //     name: '却下可能',
-  //     description: 'アラートの却下が可能か',
-  //     defaultValue: false,
-  //     control: {
-  //       type: 'boolean',
-  //     },
-  //   },
-  //   onClickDismiss: {
-  //     action: '閉じるボタンクリック',
-  //   },
-  // },
+  argTypes: {
+    // リンクフィールド
+    link: {
+      name: 'リンク',
+      description: 'リンク',
+      defaultValue: '',
+      control: {
+        type: 'text',
+      },
+    },
+    // リンク別窓フィールド
+    targetBlank: {
+      name: 'リンク別窓',
+      description: 'リンク別窓',
+      defaultValue: false,
+      control: {
+        type: 'boolean',
+      },
+    },
+  },
 };
 
 const Template = (args) => ({
-  components: { VCard },
+  components: { VCard, VImg },
   setup() {
     return { args };
   },
-  template: '<v-card v-bind="args" />',
+  template: `
+    <v-card v-bind="args">
+      <template v-slot:head>
+        <v-img src="https://picsum.photos/1000/800" :height="200" cover skeleton />
+      </template>
+      <h5 class="text-2xl font-bold tracking-tight text-gray-900">タイトル</h5>
+      <p class="mt-2 font-normal text-gray-700">テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。</p>
+      <template v-slot:foot>
+        <div class="bg-primary p-2 text-center text-sm text-white">下部エリア</div>
+      </template>
+    </v-card>
+  `,
 });
 
 export const Default = Template.bind({});
