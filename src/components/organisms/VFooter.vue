1a664123283406a1a08435ecd86944544e0d0c02
zakzakst <e1433104@gmail.com>
2022-06-28 07:56:50 +0900

    フッター作成

--- src/components/organisms/VFooter.vue
+++ src/components/organisms/VFooter.vue
@@ -1,20 +1,13 @@
 <template>
-  <footer class="p-4 bg-white rounded-lg shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800">
-    <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 <a href="https://flowbite.com/"
-        class="hover:underline">Flowbite™</a>. All Rights Reserved.
-    </span>
-    <ul class="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
-      <li>
-        <a href="#" class="mr-4 hover:underline md:mr-6 ">About</a>
-      </li>
-      <li>
-        <a href="#" class="mr-4 hover:underline md:mr-6">Privacy Policy</a>
-      </li>
-      <li>
-        <a href="#" class="mr-4 hover:underline md:mr-6">Licensing</a>
-      </li>
-      <li>
-        <a href="#" class="hover:underline">Contact</a>
+  <footer class="p-4 bg-white rounded-lg shadow md:flex md:items-center md:justify-between md:p-6">
+    <span class="text-sm text-gray-500 sm:text-center">© {{ year }} xxxxx. All Rights Reserved.</span>
+    <ul class="flex flex-wrap items-center mt-3 text-sm text-gray-500 sm:mt-0">
+      <li v-for="(link, index) in state.links" :key="index">
+        <a
+          :href="link.link"
+          class="hover:underline "
+          :class="{'mr-4 md:mr-6': index !== state.links.length - 1}"
+        >{{ link.label }}</a>
       </li>
     </ul>
   </footer>
@@ -25,5 +18,26 @@ import { reactive, computed } from 'vue';
 
 export default {
   name: 'VFooter',
+
+  setup() {
+    const state = reactive({
+      links: [
+        { link: '#', label: 'About' },
+        { link: '#', label: 'Privacy Policy' },
+        { link: '#', label: 'Licensing' },
+        { link: '#', label: 'Contact' },
+      ],
+    });
+
+    const year = computed(() => {
+      const date = new Date();
+      return date.getFullYear();
+    });
+
+    return {
+      state,
+      year,
+    };
+  },
 }
 </script>
