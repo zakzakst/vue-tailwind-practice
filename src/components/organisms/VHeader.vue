d2009209b1c661ca2b7f6f32fd400223d522621e
zakzakst <e1433104@gmail.com>
2022-07-01 12:01:28 +0900

    ヘッダー作成

--- src/components/organisms/VHeader.vue
+++ src/components/organisms/VHeader.vue
@@ -6,27 +6,38 @@
       </a>
       <div class="relative flex items-center md:order-2">
         <button
+          v-if="user"
           type="button"
           class="flex mr-3 text-sm bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300"
           @click="state.isShowUserMenu = !state.isShowUserMenu"
         >
           <span class="sr-only">{{ state.isShowUserMenu ? 'ユーザーメニューを閉じる' : 'ユーザーメニューを開く' }}</span>
-          <img class="w-8 h-8 rounded-full" :src="state.user.thumb" :alt="`${state.user.name}の写真`">
+          <img class="w-8 h-8 rounded-full" :src="user.thumb" :alt="`${user.name}の写真`">
         </button>
-        <div
-          class="absolute top-4 right-0 z-50 my-4 text-base list-none bg-white rounded divide-y divide-gray-300 shadow"
-          :class="{'hidden': !state.isShowUserMenu}"
-        >
-          <div class="py-3 px-4">
-            <span class="block text-sm text-gray-900">{{ state.user.name }}</span>
-            <span class="block text-sm font-medium text-gray-500 truncate">{{ state.user.mail }}</span>
+        <v-button
+          v-else
+          label="SIGN IN"
+          @click="onClickSignIn"
+        />
+        <transition name="v-fade" mode="out-in">
+          <div
+            v-if="user && state.isShowUserMenu"
+            class="absolute top-6 right-0 z-50 my-4 text-base list-none bg-white rounded divide-y divide-gray-300 shadow"
+          >
+            <div class="py-3 px-4">
+              <span class="block text-sm text-gray-900">{{ user.name }}</span>
+              <span class="block text-sm font-medium text-gray-500 truncate">{{ user.mail }}</span>
+            </div>
+            <ul class="py-1" aria-labelledby="dropdown">
+              <li v-for="(item, index) in state.userMenuItems" :key="index">
+                <a :href="item.link" class="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100">{{ item.label }}</a>
+              </li>
+              <li>
+                <a href="#" class="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100" @click.prevent="onClickSignOut">Sign out</a>
+              </li>
+            </ul>
           </div>
-          <ul class="py-1" aria-labelledby="dropdown">
-            <li v-for="(item, index) in state.userMenuItems" :key="index">
-              <a :href="item.link" class="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100">{{ item.label }}</a>
-            </li>
-          </ul>
-        </div>
+        </transition>
         <button
           type="button"
           class="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
@@ -34,24 +45,24 @@
         >
           <span class="sr-only">{{ state.isShowMenu ? 'メニューを閉じる' : 'メニューを開く' }}</span>
           <svg
+            v-if="!state.isShowMenu"
             class="w-6 h-6"
             fill="currentColor"
             viewBox="0 0 20 20"
-            :class="{'hidden': state.isShowMenu}"
           >
             <path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path>
           </svg>
           <svg
+            v-if="state.isShowMenu"
             class="w-6 h-6"
             fill="currentColor"
             viewBox="0 0 20 20"
-            :class="{'hidden': !state.isShowMenu}"
           >
             <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path>
           </svg>
         </button>
       </div>
-      <div class="justify-between items-center w-full md:flex md:w-auto md:order-1" :class="{'hidden': !state.isShowMenu}">
+      <div v-if="state.isShowMenu" class="justify-between items-center w-full md:flex md:w-auto md:order-1">
         <ul class="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
           <li v-for="(link, index) in state.links" :key="index">
             <a :href="link.link" class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">{{ link.label }}</a>
@@ -64,11 +75,25 @@
 
 <script>
 import { reactive } from 'vue';
+import VButton from '../atoms/VButton';
 
 export default {
   name: 'VHeader',
 
-  setup() {
+  components: {
+    VButton,
+  },
+
+  props: {
+    user: {
+      type: Object || null,
+      default: null,
+    },
+  },
+
+  emits: ['signIn', 'signOut'],
+
+  setup(props, { emit }) {
     const state = reactive({
       isShowMenu: false,
       links: [
@@ -94,11 +119,6 @@ export default {
         },
       ],
       isShowUserMenu: false,
-      user: {
-        thumb: 'https://picsum.photos/id/1005/200/200',
-        name: '山田 太郎',
-        mail: 'yamada@mail.com',
-      },
       userMenuItems: [
         {
           link: '#',
@@ -112,15 +132,21 @@ export default {
           link: '#',
           label: 'Earnings',
         },
-        {
-          link: '#',
-          label: 'Sign out',
-        },
       ],
     });
 
+    const onClickSignIn = () => {
+      emit('signIn');
+    };
+
+    const onClickSignOut = () => {
+      emit('signOut');
+    };
+
     return {
       state,
+      onClickSignIn,
+      onClickSignOut,
     };
   },
 }
--- src/stories/organisms/Header.stories.js
+++ src/stories/organisms/Header.stories.js
@@ -3,53 +3,55 @@ import VHeader from '../../components/organisms/VHeader';
 export default {
   title: 'Organisms/Header',
   component: VHeader,
-  // argTypes: {
-  //   // 表示フィールド
-  //   show: {
-  //     name: '表示',
-  //     description: '表示状態',
-  //     defaultValue: true,
-  //     control: {
-  //       type: 'boolean',
-  //     },
-  //   },
-  //   // 色フィールド
-  //   color: {
-  //     name: '色',
-  //     description: 'アラートの色',
-  //     options: [
-  //       'primary',
-  //       'secondary',
-  //       'success',
-  //       'danger',
-  //       'warning',
-  //       'info',
-  //     ],
-  //     control: {
-  //       type: 'select',
-  //       labels: {
-  //         primary: 'メインカラー',
-  //         secondary: 'セカンダリカラー',
-  //         success: '成功',
-  //         danger: '警告',
-  //         warning: '注意',
-  //         info: '補足',
-  //       },
-  //     },
-  //   },
-  //   // 却下可能フィールド
-  //   dismissible: {
-  //     name: '却下可能',
-  //     description: 'アラートの却下が可能か',
-  //     defaultValue: false,
-  //     control: {
-  //       type: 'boolean',
-  //     },
-  //   },
-  //   onClickDismiss: {
-  //     action: '閉じるボタンクリック',
-  //   },
-  // },
+  argTypes: {
+    // ユーザー情報フィールド
+    user: {
+      name: 'ユーザー情報',
+      description: 'ユーザー情報',
+      control: {
+        type: 'object',
+      },
+    },
+    // // 色フィールド
+    // color: {
+    //   name: '色',
+    //   description: 'アラートの色',
+    //   options: [
+    //     'primary',
+    //     'secondary',
+    //     'success',
+    //     'danger',
+    //     'warning',
+    //     'info',
+    //   ],
+    //   control: {
+    //     type: 'select',
+    //     labels: {
+    //       primary: 'メインカラー',
+    //       secondary: 'セカンダリカラー',
+    //       success: '成功',
+    //       danger: '警告',
+    //       warning: '注意',
+    //       info: '補足',
+    //     },
+    //   },
+    // },
+    // // 却下可能フィールド
+    // dismissible: {
+    //   name: '却下可能',
+    //   description: 'アラートの却下が可能か',
+    //   defaultValue: false,
+    //   control: {
+    //     type: 'boolean',
+    //   },
+    // },
+    onSignIn: {
+      action: 'サインイン',
+    },
+    onSignOut: {
+      action: 'サインアウト',
+    },
+  },
 };
 
 const Template = (args) => ({
@@ -62,3 +64,13 @@ const Template = (args) => ({
 
 export const Default = Template.bind({});
 Default.storyName = '通常';
+
+export const SignIn = Template.bind({});
+SignIn.storyName = 'サインイン状態';
+SignIn.args = {
+  user: {
+    thumb: 'https://picsum.photos/id/1005/200/200',
+    name: '山田 太郎',
+    mail: 'yamada@mail.com',
+  },
+};
